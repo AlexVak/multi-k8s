0a6aabcb6d22338df62806163161288c3296a940
AlexVak <avakarchuk@me.com>
2019-01-05 20:25:51 +0200

    deployment script added

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t alexvak/multi-client:latest -t alexvak/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alexvak/multi-server:latest -t alexvak/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t alexvak/multi-worker:latest -t alexvak/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push alexvak/multi-client:latest
+docker push alexvak/multi-server:latest
+docker push alexvak/multi-worker:latest
+
+docker push alexvak/multi-client:$SHA
+docker push alexvak/multi-server:$SHA
+docker push alexvak/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment server=multi-client:$SHA
+kubectl set image deployments/server-deployment server=multi-server:$SHA
+kubectl set image deployments/worker-deployment server=multi-worker:$SHA
